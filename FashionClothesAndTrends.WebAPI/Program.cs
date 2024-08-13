654e59ecf0fa860b8fab08b6ac560ca5168b282a
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2024-08-13 09:59:43 +0300

    ...

--- FashionClothesAndTrends.WebAPI/Program.cs
+++ FashionClothesAndTrends.WebAPI/Program.cs
@@ -67,6 +67,8 @@ catch (Exception e)
 
 app.Run();
 
+
+
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
