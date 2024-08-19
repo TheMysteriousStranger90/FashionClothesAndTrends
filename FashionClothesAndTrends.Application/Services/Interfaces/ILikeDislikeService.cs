a845fee586864403b3a0d9d368737560fe6a0cba
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2024-08-19 10:52:56 +0300

    add LikeDislikeService...

---
+++ FashionClothesAndTrends.Application/Services/Interfaces/ILikeDislikeService.cs
@@ -0,0 +1,11 @@
+﻿using FashionClothesAndTrends.Application.DTOs;
+
+namespace FashionClothesAndTrends.Application.Services.Interfaces;
+
+public interface ILikeDislikeService
+{
+    Task AddLikeDislikeAsync(LikeDislikeDto likeDislikeDto);
+    Task RemoveLikeDislikeAsync(Guid likeDislikeId);
+    Task<IEnumerable<LikeDislikeDto>> GetLikesDislikesByUserIdAsync(string userId);
+    Task<IEnumerable<LikeDislikeDto>> GetLikesDislikesByCommentIdAsync(Guid commentId);
+}
