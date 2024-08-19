4811a2ec27a2845ee6f2897ba58ecf2dc9be7a6a
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2024-08-19 10:30:02 +0300

    add NotificationService...

---
+++ FashionClothesAndTrends.Application/Services/Interfaces/INotificationService.cs
@@ -0,0 +1,12 @@
+﻿using FashionClothesAndTrends.Application.DTOs;
+
+namespace FashionClothesAndTrends.Application.Services.Interfaces;
+
+public interface INotificationService
+{
+    Task AddNotificationAsync(NotificationDto notificationDto);
+    Task MarkAsReadAsync(Guid notificationId);
+    Task<IEnumerable<NotificationDto>> GetNotificationsByUserIdAsync(string userId);
+    Task<IEnumerable<NotificationDto>> GetUnreadNotificationsByUserIdAsync(string userId);
+    Task<IEnumerable<NotificationDto>> GetDiscountNotificationsForWishlistAsync(Guid wishlistId);
+}
