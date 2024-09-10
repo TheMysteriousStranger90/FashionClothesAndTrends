b15d828d2d84c3bd766d21c167b037d52c471000
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2024-09-10 07:16:28 +0300

    shared and orders...

--- ClientApp/src/app/shared/shared.module.ts
+++ ClientApp/src/app/shared/shared.module.ts
@@ -3,9 +3,14 @@ import {CommonModule} from '@angular/common';
 import {MaterialModule} from './modules/material/material.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
+import { BasketSummaryComponent } from './basket-summary/basket-summary.component';
+import { OrderTotalsComponent } from './order-totals/order-totals.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BasketSummaryComponent,
+    OrderTotalsComponent
+  ],
   imports: [
     CommonModule,
     MaterialModule,
@@ -14,7 +19,9 @@ import {RouterModule} from '@angular/router';
     RouterModule,
   ],
   exports: [
-    MaterialModule
+    MaterialModule,
+    BasketSummaryComponent,
+    OrderTotalsComponent
   ]
 })
 export class SharedModule {
